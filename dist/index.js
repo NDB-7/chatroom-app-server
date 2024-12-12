4650e17b44eab3319cfe20d42ccd15f384a385fb
Nathan <52774360+NDB-7@users.noreply.github.com>
2024-12-12 07:02:05 -0800

    Chatting and username selection functional

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,37 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var socket_io_1 = require("socket.io");
-var io = new socket_io_1.Server(4000);
+var express = require("express");
+var http = require("http");
+var cors = require("cors");
+var app = express();
+var server = http.createServer(app);
+var io = new socket_io_1.Server(server, {
+    cors: {
+        origin: "*",
+    },
+});
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+// Dummy data for now. Later, it'll search the database for an active chatroom
+app.get("/rooms/:code", function (req, res) {
+    res.send({ success: true, name: "Testing Room", expiresAt: 999999 });
+});
+// Temporary map, will be replaced with database
+var userMap = new Map();
 io.on("connection", function (socket) {
+    var id = socket.id;
+    console.log("User ".concat(id, " connected"));
     socket.on("setName", function (name) {
-        console.log("User set their name to ".concat(name));
+        console.log("User ".concat(id, " set their name to ").concat(name));
+        userMap.set(id, name);
+    });
+    socket.on("sendMessage", function (message) {
+        if (userMap.has(id)) {
+            var name_1 = userMap.get(id);
+            console.log("".concat(name_1, " said ").concat(message));
+            io.emit("receiveMessage", name_1, message);
+        }
     });
 });
+server.listen(4444, function () { return console.log("Server running on port 4444"); });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,41 @@
 import { Server } from "socket.io";
+import * as express from "express";
+import * as http from "http";
+import * as cors from "cors";
 
-const io = new Server(4000);
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+
+// Dummy data for now. Later, it'll search the database for an active chatroom
+app.get("/rooms/:code", (req, res) => {
+  res.send({ success: true, name: "Testing Room", expiresAt: 999999 });
+});
+
+// Temporary map, will be replaced with database
+const userMap = new Map<string, string>();
 
 io.on("connection", socket => {
+  const id = socket.id;
+  console.log(`User ${id} connected`);
   socket.on("setName", name => {
-    console.log(`User set their name to ${name}`);
+    console.log(`User ${id} set their name to ${name}`);
+    userMap.set(id, name);
+  });
+  socket.on("sendMessage", message => {
+    if (userMap.has(id)) {
+      const name = userMap.get(id);
+      console.log(`${name} said ${message}`);
+      io.emit("receiveMessage", name, message);
+    }
   });
 });
+
+server.listen(4444, () => console.log("Server running on port 4444"));
