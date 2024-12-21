5cee0b35c8e70eda59830667cba35a57746ede5c
Nathan <52774360+NDB-7@users.noreply.github.com>
2024-12-21 11:12:21 -0800

    Shows expiration timer

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,11 @@ app.get("/rooms/:code", (req, res) => {
   const room = activeRoomsMap.get(req.params.code);
 
   if (room) {
-    res.send({ success: true, ...room.data });
+    res.send({
+      success: true,
+      name: room.data.name,
+      expiresAt: room.data.expiresAt,
+    });
   } else {
     res.status(404).send({ success: false });
   }
